fe51f353cbde63674793a31852601a32362ee78f
bellaFG <isabella.ferzales@gmail.com>
2024-09-01 11:31:05 -0300

    Ajustada a normalização das tabelas Cidade e Especialidade para evitar redundâncias e garantir integridade dos dados.

--- ClinicaMedica.sql
+++ ClinicaMedica.sql
@@ -1,3 +1,19 @@
+CREATE TABLE Medico (
+    CRM VARCHAR(10) PRIMARY KEY,
+    Nome VARCHAR(100),
+    Telefone VARCHAR(15),
+    Email VARCHAR(100),
+    Rua VARCHAR(100),
+    Numero INT,
+    Complemento VARCHAR(50),
+    Bairro VARCHAR(50),
+    CEP VARCHAR(10),
+    ID_Cidade INT,
+    ID_Especialidade INT,
+    FOREIGN KEY (ID_Cidade) REFERENCES Cidade(ID_Cidade),
+    FOREIGN KEY (ID_Especialidade) REFERENCES Especialidade(ID_Especialidade)
+);
+
 CREATE TABLE Paciente (
     CPF VARCHAR(11) PRIMARY KEY,
     Nome VARCHAR(100),
@@ -8,38 +24,31 @@ CREATE TABLE Paciente (
     Complemento VARCHAR(50),
     Bairro VARCHAR(50),
     CEP VARCHAR(10),
-    Cidade VARCHAR(50),
+    ID_Cidade INT,
+    FOREIGN KEY (ID_Cidade) REFERENCES Cidade(ID_Cidade)
+);
+
+CREATE TABLE Cidade (
+    ID_Cidade INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(50),
     Estado VARCHAR(2)
 );
 
+CREATE TABLE Especialidade (
+    ID_Especialidade INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(50)
+);
 
 CREATE TABLE Convenio (
-    ID_Convenio INT PRIMARY KEY AUTO_INCREMENT,
+    ID_Convenio INT AUTO_INCREMENT PRIMARY KEY,
     Empresa VARCHAR(100),
     Tipo VARCHAR(50),
     Vencimento DATE,
     Percentual_Coparticipacao DECIMAL(5, 2)
 );
 
-
-CREATE TABLE Medico (
-    CRM VARCHAR(10) PRIMARY KEY,
-    Especialidade VARCHAR(50),
-    Nome VARCHAR(100),
-    Telefone VARCHAR(15),
-    Email VARCHAR(100),
-    Rua VARCHAR(100),
-    Numero INT,
-    Complemento VARCHAR(50),
-    Bairro VARCHAR(50),
-    CEP VARCHAR(10),
-    Cidade VARCHAR(50),
-    Estado VARCHAR(2)
-);
-
-
 CREATE TABLE Consulta (
-    ID_Consulta INT PRIMARY KEY AUTO_INCREMENT,
+    ID_Consulta INT AUTO_INCREMENT PRIMARY KEY,
     Data DATE,
     Horario TIME,
     CRM_Medico VARCHAR(10),
@@ -47,7 +56,6 @@ CREATE TABLE Consulta (
     FOREIGN KEY (CRM_Medico) REFERENCES Medico(CRM),
     FOREIGN KEY (CPF_Paciente) REFERENCES Paciente(CPF)
 );
-
 
 CREATE TABLE Paciente_Convenio (
     CPF_Paciente VARCHAR(11),
@@ -57,8 +65,6 @@ CREATE TABLE Paciente_Convenio (
     FOREIGN KEY (ID_Convenio) REFERENCES Convenio(ID_Convenio)
 );
 
-
-
 CREATE TABLE Medico_Convenio (
     CRM_Medico VARCHAR(10),
     ID_Convenio INT,
@@ -66,3 +72,4 @@ CREATE TABLE Medico_Convenio (
     FOREIGN KEY (CRM_Medico) REFERENCES Medico(CRM),
     FOREIGN KEY (ID_Convenio) REFERENCES Convenio(ID_Convenio)
 );
+
